a16038b89f2eb14473e481b9807f6792ed5b72b3
Lane Harrison <lthtech@gmail.com>
2011-11-10 23:13:04 -0500

    first

---
+++ java/my_magnum_opus.java
@@ -0,0 +1,28 @@
+/*
+ * =================================================
+ *
+ *  Filename:
+ *  Description:
+ *
+ *  Version:    1.0
+ *  Created:  
+ *  Revision:   none
+ *  Compiler:   java
+ *  
+ *  Author: Lane Harrison, Ltharri1@uncc.edu 
+ *
+ * =================================================
+ */
+
+// Imports
+
+// Global Variables
+
+// Functions
+
+public class Main {
+        public static void main(String[] args){
+                System.out.println("Hello, World!\n");
+        }
+}
+
